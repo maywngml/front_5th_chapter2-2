28141fd865d5f954d62c1dad367630ea1b06043c
서주희 <maywngml@naver.com>
2025-04-22 03:05:32 +0900

    feat: useProduct 훅에 상품 추가 기능 구현

--- src/refactoring/hooks/useProduct.ts
+++ src/refactoring/hooks/useProduct.ts
@@ -1,10 +1,16 @@
-import { useState } from "react";
-import { Product } from "../../types.ts";
+import { useState } from 'react';
+import { Product } from '../../types.ts';
 
 export const useProducts = (initialProducts: Product[]) => {
+  const [products, setProducts] = useState<Product[]>(initialProducts);
+
+  const addProduct = (newProduct: Product) => {
+    setProducts((prevProducts) => [...prevProducts, newProduct]);
+  };
+
   return {
-    products: [],
+    products,
     updateProduct: () => undefined,
-    addProduct: () => undefined,
+    addProduct,
   };
 };
