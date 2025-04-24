c69cf199d9aa9c7576fa8825f41cfe1be8b9f096
서주희 <maywngml@naver.com>
2025-04-25 03:38:00 +0900

    chore:  배포 설정

--- vite.config.ts
+++ vite.config.ts
@@ -3,11 +3,22 @@ import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react-swc';
 import { resolve } from 'path';
 
+const isCI = process.env.CI === 'true';
+
 export default mergeConfig(
   defineConfig({
+    base: isCI ? '' : '/front_5th_chapter2-2/',
     plugins: [react()],
     resolve: {
       alias: [{ find: '@', replacement: resolve(__dirname, 'src') }],
+    },
+    build: {
+      rollupOptions: {
+        input: {
+          origin: resolve(__dirname, 'index.origin.html'),
+          refactoring: resolve(__dirname, 'index.refactoring.html'),
+        },
+      },
     },
   }),
   defineTestConfig({
