5b328fd9d376a1acb948760ab65c3db90924f8c7
서주희 <maywngml@naver.com>
2025-04-24 20:51:41 +0900

    refactor: 버튼 엘리먼트 버튼 컴포넌트로 변경

--- src/refactoring/App.tsx
+++ src/refactoring/App.tsx
@@ -1,5 +1,6 @@
 import { useState } from 'react';
 import { AdminPage, CartPage } from './pages';
+import { Button } from './components/ui';
 import { useCoupons, useProducts } from './hooks';
 import { initialCoupons } from './config/coupon';
 import { initialProducts } from './config/product';
@@ -9,17 +10,23 @@ const App = () => {
   const { coupons, addCoupon } = useCoupons(initialCoupons);
   const [isAdmin, setIsAdmin] = useState(false);
 
+  const handleClick = () => {
+    setIsAdmin((prevAdmin) => !prevAdmin);
+  };
+
   return (
     <div className='min-h-screen bg-gray-100'>
       <nav className='bg-blue-600 text-white p-4'>
         <div className='container mx-auto flex justify-between items-center'>
           <h1 className='text-2xl font-bold'>쇼핑몰 관리 시스템</h1>
-          <button
-            onClick={() => setIsAdmin(!isAdmin)}
-            className='bg-white text-blue-600 px-4 py-2 rounded hover:bg-blue-100'
+          <Button
+            className='px-4 py-2'
+            variant='solid'
+            color='white'
+            onClick={handleClick}
           >
             {isAdmin ? '장바구니 페이지로' : '관리자 페이지로'}
-          </button>
+          </Button>
         </div>
       </nav>
       <main className='container mx-auto mt-6'>
